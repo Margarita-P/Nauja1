251630e109e7dbe2f0c64a43a5340084b7525ddb
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-05-14 23:27:28 +0300

    Update person.h

--- person.h
+++ person.h
@@ -1,2 +1,23 @@
-
-h
+class Person
+{
+    protected:
+        string name;
+        string lastname;
+    public:
+        string getName() const 
+        {
+          return n;
+        }
+    string getLastName() const
+    {
+      return ln;
+    }
+    void setName(string n) 
+    {
+      name = n;
+    }
+    void setLastName(string ln)
+    {
+      lastName = ln;
+    }
+};
