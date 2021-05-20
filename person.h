4e85e2be5bc2582edc53bf8aa51cbaf5cbed87bf
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-05-20 21:38:36 +0300

    Update person.h

--- person.h
+++ person.h
@@ -1,23 +1,23 @@
-class Person
-{
+#include <string>
+
+using namespace std;
+
+class Person{
     protected:
         string name;
         string lastname;
+        double finalVid;
+	    double finalMed;
     public:
-        string getName() const 
-        {
-          return n;
-        }
-    string getLastName() const
-    {
-      return ln;
-    }
-    void setName(string n) 
-    {
-      name = n;
-    }
-    void setLastName(string ln)
-    {
-      lastName = ln;
-    }
+        string getName() const { return name; }
+		string getLastname() const { return lastname; }
+        double getFinalVid() const { return finalVid; }
+		double getFinalMed() const { return finalMed; }
+        void setName(string n) { name = n; }
+		void setLastname(string ln) { lastname = ln; }
+        void setFinalVid(double fv) { finalVid = fv; }
+		void setFinalMed(double fm) { finalMed = fm; }
+        virtual ~Person () = 0;
 };
+
+inline Person::~Person() {}
