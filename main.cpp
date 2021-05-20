8c893bf924a4993b91eada9f4fbe1bec625b161c
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-05-20 21:36:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 int main()
 {
 	int conteiner;
-	
 	string answer1, answer2;
 	string galvotukai;
 	string vargsiukai;
@@ -19,255 +18,190 @@ int main()
 		if (conteiner == 1)
 		{
 			vector <Studentas> S;
+			vector <Studentas> V;
+			vector <Studentas> G;
 			cout << "Namu darbu ir egzamino pazymiai turi priklausyti intervalui [1;10]" << endl;
-				cout << "Ar norite patys irasyti duomenis (t), ar norite, kad jie butu paimti is failo (n), ar kad keli duomenu failai butu nuskaityti? (k): ";
-				cin >> answer1;
-				if (answer1 == yes)
+			cout << "Ar norite patys irasyti duomenis (t), ar norite, kad jie butu paimti is failo (n), ar kad keli duomenu failai butu nuskaityti? (k): ";
+			cin >> answer1;
+			if (answer1 == yes)
+			{
+				cout << "Ar zinote kiek bus studentu? (t/n): ";
+				cin >> answer2;
+				if (answer2 == yes)
 				{
-					cout << "Ar zinote kiek bus studentu? (t/n): ";
-					cin >> answer2;
-					if (answer2 == yes)
+					cout << "iveskite kiek bus studentu: ";
+					cin >> n;
+					try
 					{
-						cout << "iveskite kiek bus studentu: ";
-						cin >> n;
-						try
+						if (n <= 0)
 						{
-							if (n <= 0)
-							{
-								throw 1;
-							}
-							ZinomasStudentuSK(S);
-							Print(S);
+							throw 1;
 						}
-						catch (int ex1)
-						{
-							cout << "Ivedete netinkama studentu skaiciu (skaicius > 0), paleiskite programa is naujo " << endl;
-							exit;
-						}
+						ZinomasStudentuSK(S);
+						Print2(S);
 					}
-					else if (answer2 == no)
+					catch (int ex1)
 					{
-						NezinomasStudentuSK(S);
-						Print(S);
+						cout << "Ivedete netinkama studentu skaiciu (skaicius > 0), paleiskite programa is naujo " << endl;
+						exit;
 					}
 				}
-				else if (answer1 == no)
+				else if (answer2 == no)
 				{
-					FileName = "kursiokai.txt";
+					NezinomasStudentuSK(S);
+					Print2(S);
+				}
+			}
+			else if (answer1 == no)
+			{
+				/*FileName = "kursiokai.txt";
+				ReadFromFile(S);
+				ofstream fr("rez.txt");
+				fr << "Vardas" << setw(20) << setfill(' ') << "Pavarde" << setw(30) << setfill(' ') << "Galutinis (vid)" << setw(20) << setfill(' ') << "Galutinis (med)" << endl;
+				fr << "_____________________________________________________________________________" << endl;
+				for (int i = 0; i < n; i++)
+					fr << S[i].name << setw(20) << setfill(' ') << S[i].lastname << setw(30) << setfill(' ') << setprecision(3) << S[i].finalVid << setw(20) << setfill(' ') << setprecision(3) << S[i].finalMed << setw(30) << setfill(' ') << S[i].kintamasis1 << endl;
+				fr.close();*/
+				FileName2 = "kursiokai.txt";
+				print2(S,S.size());
+			}
+			else if (answer1 == ka)
+			{
+				cout << "Kiek failu norite nuskaityti (nuo 1 iki 5)? : ";
+				cin >> kint;
+				for (int i = 0; i < kint; i++)
+				{
+					cout << "Iveskite n skaiciu: " << endl;
+                    			cin >> n;
+                    			FileName = "kursiokai" + to_string(n);
+                    			FileName += ".txt";
+                    			vargsiukai = "vargsiukai" + to_string(n);
+					vargsiukai += ".txt";
+					galvotukai = "galvotukai" + to_string(n);
+					galvotukai += ".txt";
 					ReadFromFile(S);
-					ofstream fr("rez.txt");
-					fr << "Vardas" << setw(20) << setfill(' ') << "Pavarde" << setw(30) << setfill(' ') << "Galutinis (vid)" << setw(20) << setfill(' ') << "Galutinis (med)" << endl;
-					fr << "_____________________________________________________________________________" << endl;
-					for (int i = 0; i < n; i++)
-						fr << S[i].name << setw(20) << setfill(' ') << S[i].lastname << setw(30) << setfill(' ') << setprecision(3) << S[i].finalVid << setw(20) << setfill(' ') << setprecision(3) << S[i].finalMed << setw(30) << setfill(' ') << S[i].kintamasis1 << endl;
-					fr.close();
+					Strategija1(S, G, V);
+                    			FileName2 = vargsiukai;
+                    			print2(G,G.size());
+                    			FileName2 = galvotukai;
+                    			print2(V,V.size());
 				}
-				else if (answer1 == ka)
-				{
-					cout << "Kiek failu norite nuskaityti (nuo 1 iki 5)? : ";
-					cin >> kint;
-					for (int i = 0; i < kint; i++)
-					{
-						cout << "Iveskite n skaiciu: " << endl;
-						cin >> n;
-						FileName = "kursiokai" + to_string(n);
-						FileName += ".txt";
-						vargsiukai = "vargsiukai" + to_string(n);
-						vargsiukai += ".txt";
-						galvotukai = "galvotukai" + to_string(n);
-						galvotukai += ".txt";
-						ofstream out4(galvotukai);
-						ofstream out5(vargsiukai);
-						auto start = chrono::steady_clock::now();
-						ReadFromFile(S);
-						cout << "Nuskaityti faila su " << n << " stulpeliu uztruko: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << " ms" << endl;
-						auto start1 = chrono::steady_clock::now();
-						for (int j = 0; j < n; j++)
-						{
-							if (S[j].kintamasis1 == 1)
-							{
-								out4 << S[j].name << setw(20) << setfill(' ') << S[j].lastname << setw(30) << setfill(' ') << setprecision(3) << S[j].finalVid << endl;
-							}
-							else if (S[j].kintamasis1 == 0)
-							{
-								out5 << S[j].name << setw(20) << setfill(' ') << S[j].lastname << setw(30) << setfill(' ') << setprecision(3) << S[j].finalVid << endl;
-							}
-						}
-						out4.close();
-						out5.close();
-						cout << "Atskirti studentus i Galvotukus ir Vargsiukus uztruko: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start1).count() << " ms" << endl;
-					}
-				}
+			}
 		}
 		else if (conteiner == 2)
 		{
 			list <Studentas> S;
+			list <Studentas> V;
+			list <Studentas> G;
 			cout << "Namu darbu ir egzamino pazymiai turi priklausyti intervalui [1;10]" << endl;
-				cout << "Ar norite patys irasyti duomenis (t), ar norite, kad jie butu paimti is failo (n), ar kad keli duomenu failai butu nuskaityti? (k): ";
-				cin >> answer1;
-				if (answer1 == yes)
+			cout << "Ar norite patys irasyti duomenis (t), ar norite, kad jie butu paimti is failo (n), ar kad keli duomenu failai butu nuskaityti? (k): ";
+			cin >> answer1;
+			if (answer1 == yes)
+			{
+				cout << "Ne" << endl;
+			}
+			else if (answer1 == no)
+			{
+				cout << "Ne" << endl;
+			}
+			else if (answer1 == ka)
+			{
+				cout << "Kiek failu norite nuskaityti (nuo 1 iki 5)? : ";
+				cin >> kint;
+				for (int i = 0; i < kint; i++)
 				{
-					cout << "Ar zinote kiek bus studentu? (t/n): ";
-					cin >> answer2;
-					if (answer2 == yes)
-					{
-						cout << "iveskite kiek bus studentu: ";
-						cin >> n;
-						try
-						{
-							if (n <= 0)
-							{
-								throw 1;
-							}
-							ZinomasStudentuSK(S);
-							Print(S);
-						}
-						catch (int ex1)
-						{
-							cout << "Ivedete netinkama studentu skaiciu (skaicius > 0), paleiskite programa is naujo " << endl;
-							exit;
-						}
-					}
-					else if (answer2 == no)
-					{
-						NezinomasStudentuSK(S);
-						Print(S);
-					}
-				}
-				else if (answer1 == no)
-				{
-					FileName = "kursiokai.txt";
+					cout << "Iveskite n skaiciu: " << endl;
+                    			cin >> n;
+                    			FileName = "kursiokai" + to_string(n);
+                    			FileName += ".txt";
+                    			vargsiukai = "vargsiukai" + to_string(n);
+					vargsiukai += ".txt";
+					galvotukai = "galvotukai" + to_string(n);
+					galvotukai += ".txt";
 					ReadFromFile(S);
-					ofstream fr("rez.txt");
-					fr << "Vardas" << setw(20) << setfill(' ') << "Pavarde" << setw(30) << setfill(' ') << "Galutinis (vid)" << setw(20) << setfill(' ') << "Galutinis (med)" << endl;
-					fr << "_____________________________________________________________________________" << endl;
-					for (int i = 0; i < n; i++)
-						fr << S[i].name << setw(20) << setfill(' ') << S[i].lastname << setw(30) << setfill(' ') << setprecision(3) << S[i].finalVid << setw(20) << setfill(' ') << setprecision(3) << S[i].finalMed << setw(30) << setfill(' ') << S[i].kintamasis1 << endl;
-					fr.close();
+					Strategija1(S, G, V);
+                    			FileName2 = vargsiukai;
+                    			print(G,G.size());
+                    			FileName2 = galvotukai;
+                    			print(V,V.size());
 				}
-				else if (answer1 == ka)
-				{
-					cout << "Kiek failu norite nuskaityti (nuo 1 iki 5)? : ";
-					cin >> kint;
-					for (int i = 0; i < kint; i++)
-					{
-						cout << "Iveskite n skaiciu: " << endl;
-						cin >> n;
-						FileName = "kursiokai" + to_string(n);
-						FileName += ".txt";
-						Studentas temporary;
-						vargsiukai = "vargsiukai" + to_string(n);
-						vargsiukai += ".txt";
-						galvotukai = "galvotukai" + to_string(n);
-						galvotukai += ".txt";
-						ofstream out4(galvotukai);
-						ofstream out5(vargsiukai);
-						auto start = chrono::steady_clock::now();
-						ReadFromFile1(S);
-						cout << "Nuskaityti faila su " << n << " stulpeliu uztruko: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << " ms" << endl;
-						auto start1 = chrono::steady_clock::now();
-						for (int j = 0; j < n; j++)
-						{
-							if (S.front().kintamasis1 == 1)
-							{
-								out4 << S.front().name << setw(20) << setfill(' ') << S.front().lastname << setw(30) << setfill(' ') << setprecision(3) << S.front().finalVid << endl;
-							}
-							else if (S.front().kintamasis1 == 0)
-							{
-								out5 << S.front().name << setw(20) << setfill(' ') << S.front().lastname << setw(30) << setfill(' ') << setprecision(3) << S.front().finalVid << endl;
-							}
-							S.pop_front();
-						}
-						out4.close();
-						out5.close();
-						cout << "Atskirti studentus i Galvotukus ir Vargsiukus uztruko: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start1).count() << " ms" << endl;
-					}
-				}
+			}
 		}
 		else if (conteiner == 3)
 		{
 			deque <Studentas> S;
+			deque <Studentas> G;
+			deque <Studentas> V;
 			cout << "Namu darbu ir egzamino pazymiai turi priklausyti intervalui [1;10]" << endl;
-				cout << "Ar norite patys irasyti duomenis (t), ar norite, kad jie butu paimti is failo (n), ar kad keli duomenu failai butu nuskaityti? (k): ";
-				cin >> answer1;
-				if (answer1 == yes)
+			cout << "Ar norite patys irasyti duomenis (t), ar norite, kad jie butu paimti is failo (n), ar kad keli duomenu failai butu nuskaityti? (k): ";
+			cin >> answer1;
+			if (answer1 == yes)
+			{
+			cout << "Ar zinote kiek bus studentu? (t/n): ";
+				cin >> answer2;
+				if (answer2 == yes)
 				{
-					cout << "Ar zinote kiek bus studentu? (t/n): ";
-					cin >> answer2;
-					if (answer2 == yes)
+					cout << "iveskite kiek bus studentu: ";
+					cin >> n;
+					try
 					{
-						cout << "iveskite kiek bus studentu: ";
-						cin >> n;
-						try
+						if (n <= 0)
 						{
-							if (n <= 0)
-							{
-								throw 1;
-							}
-							ZinomasStudentuSK(S);
-							Print(S);
+							throw 1;
 						}
-						catch (int ex1)
-						{
-							cout << "Ivedete netinkama studentu skaiciu (skaicius > 0), paleiskite programa is naujo " << endl;
-							exit;
-						}
-					}
-					else if (answer2 == no)
-					{
-						NezinomasStudentuSK(S);
+						ZinomasStudentuSK(S);
 						Print(S);
 					}
-				}
-				else if (answer1 == no)
-				{
-					FileName = "kursiokai.txt";
-					ReadFromFile(S);
-					ofstream fr("rez.txt");
-					fr << "Vardas" << setw(20) << setfill(' ') << "Pavarde" << setw(30) << setfill(' ') << "Galutinis (vid)" << setw(20) << setfill(' ') << "Galutinis (med)" << endl;
-					fr << "_____________________________________________________________________________" << endl;
-					for (int i = 0; i < n; i++)
-						fr << S[i].name << setw(20) << setfill(' ') << S[i].lastname << setw(30) << setfill(' ') << setprecision(3) << S[i].finalVid << setw(20) << setfill(' ') << setprecision(3) << S[i].finalMed << setw(30) << setfill(' ') << S[i].kintamasis1 << endl;
-					fr.close();
-				}
-				else if (answer1 == ka)
-				{
-					cout << "Kiek failu norite nuskaityti (nuo 1 iki 5)? : ";
-					cin >> kint;
-					for (int i = 0; i < kint; i++)
+					catch (int ex1)
 					{
-						cout << "Iveskite n skaiciu: " << endl;
-						cin >> n;
-						FileName = "kursiokai" + to_string(n);
-						FileName += ".txt";
-						vargsiukai = "vargsiukai" + to_string(n);
-						vargsiukai += ".txt";
-						galvotukai = "galvotukai" + to_string(n);
-						galvotukai += ".txt";
-						ofstream out4(galvotukai);
-						ofstream out5(vargsiukai);
-						auto start = chrono::steady_clock::now();
-						ReadFromFile(S);
-						cout << "Nuskaityti faila su " << n << " stulpeliu uztruko: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << " ms" << endl;
-						auto start1 = chrono::steady_clock::now();
-						for (int j = 0; j < n; j++)
-						{
-							if (S[j].kintamasis1 == 1)
-							{
-								out4 << S[j].name << setw(20) << setfill(' ') << S[j].lastname << setw(30) << setfill(' ') << setprecision(3) << S[j].finalVid << endl;
-							}
-							else if (S[j].kintamasis1 == 0)
-							{
-								out5 << S[j].name << setw(20) << setfill(' ') << S[j].lastname << setw(30) << setfill(' ') << setprecision(3) << S[j].finalVid << endl;
-							}
-						}
-						out4.close();
-						out5.close();
-						cout << "Atskirti studentus i Galvotukus ir Vargsiukus uztruko: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start1).count() << " ms" << endl;
+						cout << "Ivedete netinkama studentu skaiciu (skaicius > 0), paleiskite programa is naujo " << endl;
+						exit;
 					}
 				}
+				else if (answer2 == no)
+				{
+					NezinomasStudentuSK(S);
+					Print(S);
+				}
+			}
+			else if (answer1 == no)
+			{
+				/*FileName = "kursiokai.txt";
+				ReadFromFile(S);
+				ofstream fr("rez.txt");
+				fr << "Vardas" << setw(20) << setfill(' ') << "Pavarde" << setw(30) << setfill(' ') << "Galutinis (vid)" << setw(20) << setfill(' ') << "Galutinis (med)" << endl;
+				fr << "_____________________________________________________________________________" << endl;
+				for (int i = 0; i < n; i++)
+					fr << S[i].name << setw(20) << setfill(' ') << S[i].lastname << setw(30) << setfill(' ') << setprecision(3) << S[i].finalVid << setw(20) << setfill(' ') << setprecision(3) << S[i].finalMed << setw(30) << setfill(' ') << S[i].kintamasis1 << endl;
+				fr.close();
+				FileName2 = "kursiokai.txt";*/
+				print(S,S.size());
+			}
+			else if (answer1 == ka)
+			{
+				cout << "Kiek failu norite nuskaityti (nuo 1 iki 5)? : ";
+				cin >> kint;
+                		for(int i=0; i<kint; i++)
+                		{
+                    			cout << "Iveskite n skaiciu: " << endl;
+                    			cin >> n;
+                    			FileName = "kursiokai" + to_string(n);
+                    			FileName += ".txt";
+                    			vargsiukai = "vargsiukai" + to_string(n);
+					vargsiukai += ".txt";
+					galvotukai = "galvotukai" + to_string(n);
+					galvotukai += ".txt";
+					ReadFromFile(S);
+					Strategija1(S, G, V);
+                    			FileName2 = vargsiukai;
+                    			print(G,G.size());
+                    			FileName2 = galvotukai;
+                    			print(V,V.size());
+				}
+			}
 		}
 	}
+
 	catch (int ex4)
 	{
 		cout << "Ivedete netinkama skaiciu, paleiskite programa isnaujo.";
