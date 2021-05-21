79767a3eb4fb1edef24f8495d432b312149aebbd
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-05-21 21:50:07 +0300

    Update funkcija.h

--- funkcija.h
+++ funkcija.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -11,16 +10,49 @@
 #include <chrono>
 #include <list>
 #include <deque>
+#include "person.h"
 
 using namespace std;
 
-struct Studentas
+class Studentas : public Person
 {
-	string name;
-	string lastname;
-	double finalVid;
-	double finalMed;
-	int kintamasis1;
+    private:
+	    int kintamasis1;
+	
+	public:
+		Studentas()
+		{
+			Person::name = "";
+	    		Person::lastname = "";
+	   	 	Person::finalVid = 0;
+	   		Person::finalMed = 0;
+	    		kintamasis1 = 0;
+		}
+
+		int getKint1() const { return kintamasis1; }
+
+		void setKint1(int ki) { kintamasis1 = ki; }
+
+		Studentas(const Studentas& t) :
+			kintamasis1(t.kintamasis1) 
+			{
+				name = t.getName();
+				lastname = t.getLastname();
+				finalVid = t.finalVid;
+				finalMed = t.finalMed;
+			}
+
+		Studentas& operator=(const Studentas& t)
+		{
+			name = t.name;
+			lastname = t.lastname;
+			finalVid = t.finalVid;
+			finalMed = t.finalMed;
+			kintamasis1 = t.kintamasis1;
+			return *this;
+		}
+
+		~Studentas() {};
 };
 
 int sk = 0;
@@ -35,27 +67,22 @@ double number5 = 0;
 string FileName;
 vector <double> pazymiai;
 int n1 = 0, n2 = 0;
+string FileName2;
 
 bool check(double number);
 double vidurkis();
 template <class T>
 double mediana();
 template <class T>
-void ReadFromFile(T& S);
+void print(T Stud, long int a);
 template <class T>
-void ReadFromFile1(T& S);
+void print2(T Stud, long int a);
+template <class T>
+void ReadFromFile(T& S);
 template <class T>
 void ZinomasStudentuSK(T& S);
 template <class T>
 void NezinomasStudentuSK(T& S);
-template <class T>
-void Print(T& S);
 void CreateFile();
 template <class T>
 void Strategija1(T& S, T& G, T& V);
-template <class T>
-void Strategija2(T& S, T& V);
-template <class T>
-void Strategija11(T& S, T& G, T& V);
-template <class T>
-void Strategija22(T& S, T& V);
